23c5c8bfefdbbb52c83d32383cc1368ae1b1a40f
Gromojar <50329772+Gromojar@users.noreply.github.com>
2020-11-02 14:03:48 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
 
 const letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','U','W','X','Y','Z',];
 const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8','9',];
